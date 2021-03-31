8ed7d3e1cfcebb10ca74464741bfce582b5bce00
bunoviske <brunofcarvalho1996@gmail.com>
2021-03-31 11:03:59 -0300

    update image resize . every image should be smaller than 1000x1000

--- resizeImages.py
+++ resizeImages.py
@@ -3,16 +3,35 @@ from fastai.callback.all import *
 from fastai.vision.all import *
 from PIL import Image
 
-path = './database'
-fnames = get_image_files(path)
 
-
-for file in fnames:
-    basewidth = 900
+def resizeBasedOnWidth(file):
+    basewidth = 1000
     img = Image.open(file)
     wpercent = (basewidth/float(img.size[0]))
-    if wpercent <= 1:
+    if wpercent < 1:
+        print("Resize because file has big width:", file)
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth,hsize), Image.ANTIALIAS)
         img.save(file)
+
+def resizeBasedOnHeight(file):
+    baseHeight = 1000
+    img = Image.open(file)
+    hpercent = (baseHeight/float(img.size[1]))
+    if hpercent < 1:
+        print("Resize because file has big height:", file)
+        wsize = int((float(img.size[0])*float(hpercent)))
+        img = img.resize((wsize,baseHeight), Image.ANTIALIAS)
+        img.save(file)
+
+
+path = './database'
+fnames = get_image_files(path)
+
+print("Resizing big images...")
+for file in fnames:
+    resizeBasedOnWidth(file)
+    resizeBasedOnHeight(file)
+
+
 
